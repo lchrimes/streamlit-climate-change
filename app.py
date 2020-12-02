fd75bfab3808444c7ed4889601beb413511cdc08
Luke Warm <lukewarm@Lukes-MacBook-Pro.local>
2020-12-02 20:31:17 +0000

    removed the division of 2000

--- app.py
+++ app.py
@@ -151,22 +151,22 @@ if age != 0:
   trees_needed = round(sum(total_carbon_locked)/y[0])
 
   # Total Cost of Sequestering Carbon
-  tons_sequestered = sum(total_carbon_locked)/2000
+  tons_sequestered = sum(total_carbon_locked)
   cost_of_trees = trees_needed * 0.5
   cost_of_fill = tons_sequestered * 60
 
   # Carbon written stats displays
   execute_markdown("<p> Intertesing Facts with your data:</p>")
-  co2_released = tons_sequestered * 3.67
-  if round(co2_released) < 0.0001:
+  co2_total = tons_sequestered * 3.6 # relative amount of co2
+  if round(co2_total,3) < 0.001:
     execute_markdown(f"<li>Cutting down this tree would lead to < 0.001 tons of CO2 being released </li>")
   else: 
-    execute_markdown(f"<li>Cutting down this tree would lead to {round(co2_released,3)} tons of CO2 being released </li>")
+    execute_markdown(f"<li>Cutting down this tree would lead to {round(co2_total,3)} tons of CO2 being released </li>")
   execute_markdown(f"<li>To off-set this you would need to plant {trees_needed} tree, costing £{round(cost_of_trees,2)}</li>")
   execute_markdown(f"<li>Or the cost of capturing carbon manually is £{round(cost_of_fill,2)}</li>")
 
   # Expander for the tree area
-  tree_expander = st.beta_expander("Trees Cut Down")
+  tree_expander = st.beta_expander("Visualise the trees that need planting")
   with tree_expander:
     st.markdown(tree_icons(trees_needed), unsafe_allow_html=True)
 
