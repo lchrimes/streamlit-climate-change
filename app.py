ae82eace324c0bc544c03e11090998cea6f876e0
databounduk <61665988+databounduk@users.noreply.github.com>
2021-06-04 08:49:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ carbon_sequestered_df = load_carbon_data()
 st.header("The Great Carbon Sink")
 
 # Article Image
-st.image("https://cdn.pixabay.com/photo/2015/12/01/20/28/green-1072828_1280.jpg", use_column_width=True)
+# st.image("https://cdn.pixabay.com/photo/2015/12/01/20/28/green-1072828_1280.jpg", use_column_width=True)
 
 # Intro
 execute_markdown(open("static/html/intro.html","r").read())
