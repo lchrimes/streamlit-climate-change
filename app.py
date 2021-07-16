297a647fe06942df28f31017ac3974195b4127da
lchrimes <lchrimes@databound.co.uk>
2021-07-16 09:17:45 +0100

    updater for heroku

--- app.py
+++ app.py
@@ -8,67 +8,77 @@ import plotly.express as px
 from PIL import Image
 
 # Changing title page and the favicon - set page call only be called once per app and only at the start
-st.set_page_config(page_title='Climate Awareness')
+st.set_page_config(page_title="Climate Awareness")
 
 # Loading carbon data
 @st.cache()
 def load_carbon_data():
-  carbonSequesteredDf = pd.read_csv("static/data/TheGreatCarbonSinkInfo-Carbon-Rate.csv")
-  return carbonSequesteredDf
+    carbonSequesteredDf = pd.read_csv(
+        "static/data/TheGreatCarbonSinkInfo-Carbon-Rate.csv"
+    )
+    return carbonSequesteredDf
+
 
 # Static data
 life_span = {
-"Maple" : 300,
-"Oak" : 1000,
-"Sycamore" : 600,
-"Pine" : 200,
-"Fir" : 1000,
-"Elm" : 150,
-"Willow" : 75,
-"Magnolia" : 120,
-"Birch" : 50,
-"Tulip" : 500,
-"Butternut" : 75,
-"Cedar" : 1000,
+    "Maple": 300,
+    "Oak": 1000,
+    "Sycamore": 600,
+    "Pine": 200,
+    "Fir": 1000,
+    "Elm": 150,
+    "Willow": 75,
+    "Magnolia": 120,
+    "Birch": 50,
+    "Tulip": 500,
+    "Butternut": 75,
+    "Cedar": 1000,
 }
 
 total_sequestered = {
-  'Maple': 54,
- 'Oak': 1450,
- 'Sycamore': 348,
- 'Pine': 19,
- 'Fir': 1450,
- 'Elm': 10,
- 'Willow': 2,
- 'Magnolia': 6,
- 'Birch': 1,
- 'Tulip': 211,
- 'Butternut': 2,
-  'Cedar': 1450}
+    "Maple": 54,
+    "Oak": 1450,
+    "Sycamore": 348,
+    "Pine": 19,
+    "Fir": 1450,
+    "Elm": 10,
+    "Willow": 2,
+    "Magnolia": 6,
+    "Birch": 1,
+    "Tulip": 211,
+    "Butternut": 2,
+    "Cedar": 1450,
+}
 
 carbon_metrics = {
-    "House Build" : 65,
-    "General consumerism" : 16,
-    "Family flight to Spain" : 10,
-    "Meat based diet" : 12,
-    "UK average mileage" : 4.6,
-    "Gas central heating" : 2.25,
-    "Electricity" : 0.75,
-    "Water supply" : 0.2
+    "House Build": 65,
+    "General consumerism": 16,
+    "Family flight to Spain": 10,
+    "Meat based diet": 12,
+    "UK average mileage": 4.6,
+    "Gas central heating": 2.25,
+    "Electricity": 0.75,
+    "Water supply": 0.2,
 }
 
 # Adding sytle sheet
 def remote_css(url):
-  st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+
 
 def execute_markdown(markdown):
-  st.markdown(markdown, unsafe_allow_html=True)
+    st.markdown(markdown, unsafe_allow_html=True)
+
 
 # font-awsome addition for icons
-remote_css("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")
+remote_css(
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+)
 
 # Add some bootstrap css
-execute_markdown('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">')
+execute_markdown(
+    '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'
+)
 
 # hiding the streamlit menu and footer
 hide_streamlit_style = """
@@ -77,7 +87,7 @@ hide_streamlit_style = """
 footer {visibility: hidden;}
 </style>
 """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Adding the logo
 st.image("static/images/app_logo.png", use_column_width=True)
@@ -89,75 +99,89 @@ carbon_sequestered_df = load_carbon_data()
 st.header("The Great Carbon Sink")
 
 # Article Image
-st.image("https://cdn.pixabay.com/photo/2015/12/01/20/28/green-1072828_1280.jpg", use_column_width=True)
+st.image(
+    "https://cdn.pixabay.com/photo/2015/12/01/20/28/green-1072828_1280.jpg",
+    use_column_width=True,
+)
 
 # Intro
-execute_markdown(open("static/html/intro.html","r").read())
+execute_markdown(open("static/html/intro.html", "r").read())
 
 # User input into columns
-c1 , c2 = st.beta_columns((2,2))
+c1, c2 = st.beta_columns((2, 2))
 
-with c1: # user input to col one
-  age = st.number_input("What is the age of your tree?",  step=1, value=0)
-with c2: # user input to col two
-  tree = st.selectbox(
-        "Tree Type", # Label
-        [ # Tree type list
-        "Maple",
-        "Oak",
-        "Sycamore",
-        "Pine",
-        "Fir",
-        "Elm",
-        "Willow",
-        "Magnolia",
-        "Birch",
-        "Tulip",
-        "Cedar"
-        ]
-                    )
+with c1:  # user input to col one
+    age = st.number_input("What is the age of your tree?", step=1, value=0)
+with c2:  # user input to col two
+    tree = st.selectbox(
+        "Tree Type",  # Label
+        [  # Tree type list
+            "Maple",
+            "Oak",
+            "Sycamore",
+            "Pine",
+            "Fir",
+            "Elm",
+            "Willow",
+            "Magnolia",
+            "Birch",
+            "Tulip",
+            "Cedar",
+        ],
+    )
 
-# Tree icons and weigth icons 
+# Tree icons and weigth icons
 def tree_icons(number):
-  icons = '<i class="fa fa-tree" style="font-size:48px;"></i>' * number
-  return icons
+    icons = '<i class="fa fa-tree" style="font-size:48px;"></i>' * number
+    return icons
+
 
 # If user input do graphs
 if age != 0:
 
-  # Sample data
-  x = np.arange(60)
-  y = carbon_sequestered_df[tree].values * 0.00045359237 # all values are being converted to tons
+    # Sample data
+    x = np.arange(60)
+    y = (
+        carbon_sequestered_df[tree].values * 0.00045359237
+    )  # all values are being converted to tons
 
-  # Fit with polyfit
-  b, m= polyfit(x, y, 1)
-  p = np.poly1d(np.polyfit(x, y, 2))
-  
-  # Get the total carbon sequestered based on the polynomial generated from the known data
-  total_carbon_locked = []
-  for i in range(0,age):
-    total_carbon_locked.append(p(i))
-  trees_needed = round(sum(total_carbon_locked)/y[0])
+    # Fit with polyfit
+    b, m = polyfit(x, y, 1)
+    p = np.poly1d(np.polyfit(x, y, 2))
 
-  # Total Cost of Sequestering Carbon
-  tons_sequestered = sum(total_carbon_locked)
-  cost_of_trees = trees_needed * 0.5
-  cost_of_fill = tons_sequestered * 60
+    # Get the total carbon sequestered based on the polynomial generated from the known data
+    total_carbon_locked = []
+    for i in range(0, age):
+        total_carbon_locked.append(p(i))
+    trees_needed = round(sum(total_carbon_locked) / y[0])
 
-  # Carbon written stats displays
-  execute_markdown("<p> Intertesting Facts with your data:</p>")
-  co2_total = tons_sequestered * 3.6 # relative amount of co2
-  if round(co2_total,3) < 0.001:
-    execute_markdown(f"<li>Cutting down this tree would lead to < 0.001 tons of CO2 being released </li>")
-  else: 
-    execute_markdown(f"<li>Cutting down this tree would lead to {round(co2_total,3)} tons of CO2 being released </li>")
-  execute_markdown(f"<li>To off-set this you would need to plant {trees_needed} tree(s), costing £{round(cost_of_trees,2)}</li>")
-  execute_markdown(f"<li>Or to capture the carbon manually would cost approx £{round(cost_of_fill,2)}</li>")
+    # Total Cost of Sequestering Carbon
+    tons_sequestered = sum(total_carbon_locked)
+    cost_of_trees = trees_needed * 0.5
+    cost_of_fill = tons_sequestered * 60
 
-  # Expander for the tree area
-  tree_expander = st.beta_expander("Visualising the saplings")
-  with tree_expander:
-    st.markdown(tree_icons(trees_needed), unsafe_allow_html=True)
+    # Carbon written stats displays
+    execute_markdown("<p> Intertesting Facts with your data:</p>")
+    co2_total = tons_sequestered * 3.6  # relative amount of co2
+    if round(co2_total, 3) < 0.001:
+        execute_markdown(
+            f"<li>Cutting down this tree would lead to < 0.001 tons of CO2 being released </li>"
+        )
+    else:
+        execute_markdown(
+            f"<li>Cutting down this tree would lead to {round(co2_total,3)} tons of CO2 being released </li>"
+        )
+    execute_markdown(
+        f"<li>To off-set this you would need to plant {trees_needed} tree(s), costing £{round(cost_of_trees,2)}</li>"
+    )
+    execute_markdown(
+        f"<li>Or to capture the carbon manually would cost approx £{round(cost_of_fill,2)}</li>"
+    )
+
+    # Expander for the tree area
+    tree_expander = st.beta_expander("Visualising the saplings")
+    with tree_expander:
+        st.markdown(tree_icons(trees_needed), unsafe_allow_html=True)
 
 # Carbon Sequestered by all tree types
 fig = go.Figure()
@@ -166,64 +190,74 @@ for tree in carbon_sequestered_df.columns[1:]:
     x = np.arange(60)
     y = carbon_sequestered_df[tree].values * 0.00045359237
 
-    fig.add_trace(go.Scatter(
-        x=x,
-        y=y,
-        name=tree
-    ))
+    fig.add_trace(go.Scatter(x=x, y=y, name=tree))
 
 fig.update_layout(
-  title=f"Average Carbon Sequestered Annually",
-  xaxis_title="Tree Age (years)",
-  yaxis_title="Carbon Sequestered (tonne)"
+    title=f"Average Carbon Sequestered Annually",
+    xaxis_title="Tree Age (years)",
+    yaxis_title="Carbon Sequestered (tonne)",
 )
 st.plotly_chart(fig, use_container_width=True)
 
 # Total carbon sequestered over life time
-total_sequestered_values = np.array(list(total_sequestered.values()))* 0.00045359237
-sequestered_data = pd.DataFrame({"Tree" : total_sequestered.keys() , "Total Sequestered" : total_sequestered_values})
+total_sequestered_values = np.array(list(total_sequestered.values())) * 0.00045359237
+sequestered_data = pd.DataFrame(
+    {"Tree": total_sequestered.keys(), "Total Sequestered": total_sequestered_values}
+)
 
-fig = px.bar(sequestered_data, x='Tree', y='Total Sequestered', log_y=True)
+fig = px.bar(sequestered_data, x="Tree", y="Total Sequestered", log_y=True)
 fig.update_layout(
-  title="Estimated Maximal Carbon Sequestered Over Lifespan",
-  xaxis_title="Tree Type",
-  yaxis_title="Carbon Sequestered log(tonne)",
-  plot_bgcolor='rgba(0,0,0,0)',
-  )
-fig.update_xaxes(showline=True, linewidth=2, linecolor='#B4B1B0', gridcolor='#B4B1B0')
-fig.update_yaxes(showline=True, linewidth=2, linecolor='#B4B1B0', gridcolor='#B4B1B0')
+    title="Estimated Maximal Carbon Sequestered Over Lifespan",
+    xaxis_title="Tree Type",
+    yaxis_title="Carbon Sequestered log(tonne)",
+    plot_bgcolor="rgba(0,0,0,0)",
+)
+fig.update_xaxes(showline=True, linewidth=2, linecolor="#B4B1B0", gridcolor="#B4B1B0")
+fig.update_yaxes(showline=True, linewidth=2, linecolor="#B4B1B0", gridcolor="#B4B1B0")
 st.plotly_chart(fig, use_container_width=True)
 
 # The average household
 st.header("The Average Household")
-execute_markdown(open("static/html/carbon_footprint.html","r").read())
+execute_markdown(open("static/html/carbon_footprint.html", "r").read())
 
-carbon_average  = pd.DataFrame({"Consumption" : carbon_metrics.keys() , "Carbon Emissions (tonne)" : carbon_metrics.values()})
-fig = px.bar(carbon_average, x='Consumption', y='Carbon Emissions (tonne)')
+carbon_average = pd.DataFrame(
+    {
+        "Consumption": carbon_metrics.keys(),
+        "Carbon Emissions (tonne)": carbon_metrics.values(),
+    }
+)
+fig = px.bar(carbon_average, x="Consumption", y="Carbon Emissions (tonne)")
 fig.update_layout(
-  title="Yearly UK Average Household Carbon Emissions",
-  plot_bgcolor='rgba(0,0,0,0)',
-  )
-fig.update_xaxes(showline=True, linewidth=2, linecolor='#B4B1B0', gridcolor='#B4B1B0')
-fig.update_yaxes(showline=True, linewidth=2, linecolor='#B4B1B0', gridcolor='#B4B1B0')
+    title="Yearly UK Average Household Carbon Emissions", plot_bgcolor="rgba(0,0,0,0)",
+)
+fig.update_xaxes(showline=True, linewidth=2, linecolor="#B4B1B0", gridcolor="#B4B1B0")
+fig.update_yaxes(showline=True, linewidth=2, linecolor="#B4B1B0", gridcolor="#B4B1B0")
 st.plotly_chart(fig, use_container_width=True)
 
 # Impact Paragraph
 st.header("Our Impact Can Be Felt")
-execute_markdown(open("static/html/impact.html","r").read())
+execute_markdown(open("static/html/impact.html", "r").read())
 
 # Oxford Graphics - Global CO2 emissions
-execute_markdown('<iframe src="https://ourworldindata.org/grapher/co-emissions-per-capita?tab=chart&stackMode=absolute&region=World" loading="lazy" style="width: 100%; height: 600px; border: 0px none;"></iframe>')
+execute_markdown(
+    '<iframe src="https://ourworldindata.org/grapher/co-emissions-per-capita?tab=chart&stackMode=absolute&region=World" loading="lazy" style="width: 100%; height: 600px; border: 0px none;"></iframe>'
+)
 
-# Oxford Graphic - Global 
-execute_markdown('<iframe src="https://ourworldindata.org/grapher/annual-deforestation?stackMode=absolute&region=World" loading="lazy" style="width: 100%; height: 600px; border: 0px none;"></iframe>')
+# Oxford Graphic - Global
+execute_markdown(
+    '<iframe src="https://ourworldindata.org/grapher/annual-deforestation?stackMode=absolute&region=World" loading="lazy" style="width: 100%; height: 600px; border: 0px none;"></iframe>'
+)
 
 # Sponsor
-execute_markdown(open("static/html/sponsorship.html","r").read())
+execute_markdown(open("static/html/sponsorship.html", "r").read())
 
 # Sponsorship button to nav to site
-link = '[Click here to donate](https://www.woodlandtrust.org.uk/support-us/give/donations/)'
+link = "[Click here to donate](https://www.woodlandtrust.org.uk/support-us/give/donations/)"
 st.markdown(link, unsafe_allow_html=True)
 
 # Sponsorship logo
-st.image("https://thelandtrust.org.uk/wp-content/uploads/2015/09/Woodland-Trust.png", use_column_width=True)
+st.image(
+    "https://thelandtrust.org.uk/wp-content/uploads/2015/09/Woodland-Trust.png",
+    use_column_width=True,
+)
+
